787dbd6e51038f4f17752c0dc041f94da9f1991a
akbar <a.akbar.tjk99@gmail.com>
2022-09-28 10:00:50 +0500

    skiping InsecureSkipVerify Transport

--- options.go
+++ options.go
@@ -1,12 +1,19 @@
 package swift
 
-import "net/http"
+import (
+	"crypto/tls"
+	"net/http"
+)
 
 // Option is a function that sets some option on API.
 type Option func(api *API)
 
 // HTTPClient creates new option for using custom http.Client
 func HTTPClient(client *http.Client) Option {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client.Transport = tr
 	return func(api *API) {
 		api.httpClient = client
 	}
